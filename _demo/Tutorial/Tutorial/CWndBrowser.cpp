008f8e8c55f353a9ef72d7d13f22cfecedcd82c0
Alberl <316293804@qq.com>
2023-01-07 12:56:37 +0800

    新增wke窗口的demo

--- _demo/Tutorial/Tutorial/CWndBrowser.cpp
+++ _demo/Tutorial/Tutorial/CWndBrowser.cpp
@@ -1,39 +1,14 @@
+#include "CWndBrowser.h"
+
 #include <windows.h>
 #include <Shlwapi.h>
 #include <xstring>
 #include <vector>
-#include "CWndBrowser.h"
+
+#include "framework.h"
 #include "Resource.h"
 #include "HideWndHelp.h"
 
-std::string utf16ToUtf8(LPCWSTR lpszSrc)
-{
-    std::string sResult;
-    if (lpszSrc != NULL) {
-        int  nUTF8Len = WideCharToMultiByte(CP_UTF8, 0, lpszSrc, -1, NULL, 0, NULL, NULL);
-        char* pUTF8 = new char[nUTF8Len + 1];
-        if (pUTF8 != NULL) {
-            ZeroMemory(pUTF8, nUTF8Len + 1);
-            WideCharToMultiByte(CP_UTF8, 0, lpszSrc, -1, pUTF8, nUTF8Len, NULL, NULL);
-            sResult = pUTF8;
-            delete[] pUTF8;
-        }
-    }
-    return sResult;
-}
-
-std::wstring utf8ToUtf16(const std::string& utf8)
-{
-    std::wstring utf16;
-    size_t n = ::MultiByteToWideChar(CP_UTF8, 0, utf8.c_str(), utf8.size(), nullptr, 0);
-    if (0 == n)
-        return L"";
-    std::vector<wchar_t> wbuf(n);
-    ::MultiByteToWideChar(CP_UTF8, 0, utf8.c_str(), utf8.size(), &wbuf[0], n);
-    utf16.resize(n + 5);
-    utf16.assign(&wbuf[0], n);
-    return utf16;
-}
 
 bool saveDumpFile(const std::wstring& url, const char* buffer, unsigned int size)
 {
@@ -678,8 +653,8 @@ LRESULT CALLBACK CWndBrowser::editWndProc(HWND hWnd, UINT message, WPARAM wParam
 mbWebView CWndBrowser::createWnd()
 {
     RECT startRect;
-    startRect.left = 400;
-    startRect.top = 200;
+    startRect.left = 200;
+    startRect.top = 100;
     startRect.right = startRect.left + 1080;
     startRect.bottom = startRect.top + 680;
     const DWORD dwStyle = WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN;
--- _demo/Tutorial/Tutorial/Tutorial.cpp
+++ _demo/Tutorial/Tutorial/Tutorial.cpp
@@ -8,6 +8,7 @@
 #include "../../../mbvip/core/mb.h"
 #include "CWndSimple.h"
 #include "CWndBrowser.h"
+#include "CWndChart.h"
 
 
 #define MAX_LOADSTRING 100
@@ -21,6 +22,100 @@ WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//
+//  函数: MyRegisterClass()
+//
+//  目标: 注册窗口类。
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_TUTORIAL));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    //wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_TUTORIAL);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   函数: InitInstance(HINSTANCE, int)
+//
+//   目标: 保存实例句柄并创建主窗口
+//
+//   注释:
+//
+//        在此函数中，我们在全局变量中保存实例句柄并
+//        创建和显示主程序窗口。
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+    hInst = hInstance; // 将实例句柄存储在全局变量中
+
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+    if (!hWnd)
+    {
+        return FALSE;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    return TRUE;
+}
+
+//
+//  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  目标: 处理主窗口的消息。
+//
+//  WM_COMMAND  - 处理应用程序菜单
+//  WM_PAINT    - 绘制主窗口
+//  WM_DESTROY  - 发送退出消息并返回
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        // TODO: 在此处添加使用 hdc 的任何绘图代码...
+
+        RECT rcClient;
+        ::GetClientRect(hWnd, &rcClient);
+
+        HBRUSH hbr = (HBRUSH)::GetStockObject(GRAY_BRUSH); // 绘制背景色
+        ::FillRect(hdc, &rcClient, hbr);
+
+        EndPaint(hWnd, &ps);
+    }
+    break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
 
 void createWndTrans()
 {
@@ -65,6 +160,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		return 0;
 	}
 
+    // 这个是wke窗口，跟mb窗口不兼容，不能调用mbInit，不然不能初始化wke
+    createWndChart();
+    return 0;
+
     mbSettings settings;
     memset(&settings, 0, sizeof(settings));
     //settings.mask = MB_ENABLE_NODEJS;
@@ -73,7 +172,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     // 显示mb窗口
     //createWndTrans(); 
-    createWndSimple();
+    //createWndSimple();
     //createWndBrowser();
 
     // 显示win32窗口
@@ -97,97 +196,3 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	return (int) msg.wParam;
 }
 
-
-
-//
-//  函数: MyRegisterClass()
-//
-//  目标: 注册窗口类。
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-    WNDCLASSEXW wcex;
-
-    wcex.cbSize = sizeof(WNDCLASSEX);
-
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_TUTORIAL));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    //wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_TUTORIAL);
-    wcex.lpszMenuName   = NULL;
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-    return RegisterClassExW(&wcex);
-}
-
-//
-//   函数: InitInstance(HINSTANCE, int)
-//
-//   目标: 保存实例句柄并创建主窗口
-//
-//   注释:
-//
-//        在此函数中，我们在全局变量中保存实例句柄并
-//        创建和显示主程序窗口。
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-   hInst = hInstance; // 将实例句柄存储在全局变量中
-
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-
-   if (!hWnd)
-   {
-      return FALSE;
-   }
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return TRUE;
-}
-
-//
-//  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  目标: 处理主窗口的消息。
-//
-//  WM_COMMAND  - 处理应用程序菜单
-//  WM_PAINT    - 绘制主窗口
-//  WM_DESTROY  - 发送退出消息并返回
-//
-//
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    switch (message)
-    {
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: 在此处添加使用 hdc 的任何绘图代码...
-
-			RECT rcClient;
-			::GetClientRect(hWnd, &rcClient);
-
-			HBRUSH hbr = (HBRUSH)::GetStockObject(GRAY_BRUSH); // 绘制背景色
-			::FillRect(hdc, &rcClient, hbr);
-
-			EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
-}
